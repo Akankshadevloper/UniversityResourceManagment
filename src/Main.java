c89462e16539ea791687e218f7b9386901c07c5c
Akankshadevloper <110593751+Akankshadevloper@users.noreply.github.com>
2023-12-10 20:12:37 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Sample usage of the University Management System
-
-        // Creating students
+        
         Student student1 = new Student("S001", "Akanksha Kumari");
         Student student2 = new Student("S002", "Preety");
 
-        // Creating teachers
+       
         Teacher teacher1 = new Teacher("T001", "Professor Ravi ");
 
-        // Creating courses
+        
         Course mathCourse = new Course("MATH101", "Introduction to Mathematics", 2);
         Course javaCourse = new Course("CS101", "Java Programming", 3);
 
-        // Enrolling students and assigning teachers to courses
+        
         mathCourse.enrollStudent(student1);
         mathCourse.enrollStudent(student2);
         javaCourse.enrollStudent(student1);
@@ -23,12 +21,12 @@ public class Main {
         mathCourse.addTeacher(teacher1);
         javaCourse.addTeacher(teacher1);
 
-        // Displaying enrolled courses for students and taught courses for teachers
+        
         student1.displayEnrolledCourses();
         student2.displayEnrolledCourses();
         teacher1.displayTaughtCourses();
 
-        // Conducting exams and generating reports
+        
         mathCourse.conductExam();
         javaCourse.conductExam();
 
